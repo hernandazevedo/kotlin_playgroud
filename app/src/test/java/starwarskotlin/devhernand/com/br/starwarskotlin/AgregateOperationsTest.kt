fec2844dfc63ae151f7da3403e39a94c54beffdd
Hernand <hernand.azevedo@gmail.com>
2017-10-22 12:42:40 -0200

    Agregate operations

--- app/src/test/java/starwarskotlin/devhernand/com/br/starwarskotlin/AgregateOperationsTest.kt
+++ app/src/test/java/starwarskotlin/devhernand/com/br/starwarskotlin/AgregateOperationsTest.kt
@@ -14,7 +14,7 @@ class AgregateOperationsTest {
     Returns true if at least one element matches the given predicate.
      */
     @Test
-    fun any_operation(){
+    fun anyTest(){
         assertTrue(list.any { it % 2 == 0 })
         assertFalse(list.any { it > 10 })
     }
@@ -22,7 +22,7 @@ class AgregateOperationsTest {
     Returns true if all the elements match the given predicate.
      */
     @Test
-    fun all_operation(){
+    fun allTest(){
         assertTrue(list.all { it < 10 })
         assertFalse(list.all { it % 2 == 0 })
     }
@@ -31,7 +31,7 @@ class AgregateOperationsTest {
     Returns the number of elements matching the given predicate.
      */
     @Test
-    fun count_operation(){
+    fun countTest(){
         assertEquals(3,list.count{ it%2 == 0})
     }
 
@@ -40,7 +40,7 @@ class AgregateOperationsTest {
     the first to the last element in a collection.
      */
     @Test
-    fun fold_operation(){
+    fun foldTest(){
         assertEquals(25, list.fold(4) { total, next -> total + next })
     }
 
@@ -48,7 +48,93 @@ class AgregateOperationsTest {
     Same as fold, but it goes from the last element to first.
      */
     @Test
-    fun foldRight_operation(){
+    fun foldRightTest(){
         assertEquals(25, list.foldRight(4) { total, next -> total + next })
+    }
+
+    /*
+    Performs the given operation to each element.
+     */
+    @Test
+    fun forEachTest(){
+        list.forEach { println(it) }
+    }
+
+    /*
+    Same as forEach, though we also get the index of the element.
+     */
+    @Test
+    fun forEachIndexedTest(){
+        list.forEachIndexed { index, value ->
+            println("position $index contains a $value")
+        }
+    }
+
+    /*
+    Returns the largest element or null if there are no elements.
+     */
+    @Test
+    fun maxTest(){
+        assertEquals(6,list.max())
+    }
+
+    /*
+    Returns the first element yielding the largest value of the given function or null if
+    there are no elements.
+     */
+    @Test
+    fun maxByTest(){
+        // The element whose negative is greater
+        assertEquals(1, list.maxBy { -it })
+    }
+
+    /*
+    Returns the smallest element or null if there are no elements.
+     */
+    @Test
+    fun minTest(){
+        assertEquals(1, list.min())
+    }
+
+    /*
+    Returns the first element yielding the smallest value of the given function or null if
+    there are no elements.
+     */
+    @Test
+    fun minByTest(){
+        assertEquals(6,list.minBy { -it })
+    }
+
+    /*
+    Returns true if no elements match the given predicate.
+     */
+    @Test
+    fun noneTest(){
+        assertTrue(list.none{it % 7 == 0})
+    }
+
+    /*
+    Same as fold, but it doesn’t use an initial value. It accumulates the value applying
+    an operation from the first to the last element in a collection.
+     */
+    @Test
+    fun reduceTest(){
+        assertEquals(21, list.reduce { total, next -> total + next })
+    }
+
+    /*
+    Same as reduce, but it goes from the last element to first.
+    */
+    @Test
+    fun reduceRightTest(){
+        assertEquals(21, list.reduceRight { total, next -> total + next })
+    }
+    /*
+    Returns the sum of all values produced by the transform function from the elements
+    in the collection.
+     */
+    @Test
+    fun sumByTest(){
+        assertEquals(3, list.sumBy { it % 2 })
     }
 }
