3d6dd723f5cdb30af007ad2f9bacdfa42f17d41c
Hernand <hernand.azevedo@gmail.com>
2017-10-22 17:09:48 -0200

    Filtering operations

--- app/src/test/java/starwarskotlin/devhernand/com/br/starwarskotlin/FilteringOperationsTest.kt
+++ app/src/test/java/starwarskotlin/devhernand/com/br/starwarskotlin/FilteringOperationsTest.kt
@@ -1,5 +1,21 @@
 package starwarskotlin.devhernand.com.br.starwarskotlin
 
+import junit.framework.Assert.assertEquals
+import org.junit.Assert
+import org.junit.Test
+
 /**
  * Created by Nando on 22/10/2017.
  */
+class FilteringOperationsTest{
+
+    val list = listOf(1, 2, 3, 4, 5, 6)
+
+    /**
+     * Returns a list containing all elements except first n elements.
+     */
+    @Test
+    fun dropTest() {
+        assertEquals(listOf(5, 6), list.drop(4))
+    }
+}
